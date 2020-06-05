fc1c42319c490dfcd4087887d2df2d7bfb0af1bd
wanderer06 <georgica_fara_de_frica@yahoo.com>
2020-06-05 11:58:59 +0100

    Add ui helpers

--- XPercent/Main.lua
+++ XPercent/Main.lua
@@ -1,11 +1,46 @@
 import 'Turbine'
 import 'Turbine.Gameplay'
-import 'XpPercent.LevelXp'
+import 'XPercent.LevelXp'
+import 'Turbine.UI'
+import 'Turbine.UI.Lotro'
 
-Turbine.Shell.WriteLine('XpPercent is running and tracking your XP!')
+Turbine.Shell.WriteLine('XPercent is running and tracking your XP!')
+
+window = Turbine.UI.Window()
+window:SetSize(100, 44)
+window:SetBackColor(Turbine.UI.Color(0, 0, 0, 0))
+window:SetBlendMode(Turbine.UI.BlendMode.AlphaBlend)
+-- window:SetText('Level Percentage')
+window:SetVisible(true)
+window:SetPosition(Turbine.UI.Display:GetWidth() - 320, 20)
+
+totalXpLabel = Turbine.UI.Label()
+totalXpLabel:SetParent(window)
+totalXpLabel:SetVisible(true)
+totalXpLabel:SetSize(100, 22)
+totalXpLabel:SetFont(Turbine.UI.Lotro.Font.Verdana16)
+totalXpLabel:SetTextAlignment(Turbine.UI.ContentAlignment.MiddleRight)
+totalXpLabel:SetFontStyle(Turbine.UI.FontStyle.Outline);
+totalXpLabel:SetForeColor(Turbine.UI.Color(0.84, 1, 1, 1))
+totalXpLabel:SetOutlineColor(Turbine.UI.Color(0, 0, 0))
+totalXpLabel:SetText('0')
+
+diffXpLabel = Turbine.UI.Label()
+diffXpLabel:SetParent(window)
+diffXpLabel:SetVisible(true)
+diffXpLabel:SetSize(100, 22)
+diffXpLabel:SetPosition(0, 16)
+diffXpLabel:SetFont(Turbine.UI.Lotro.Font.Verdana16)
+diffXpLabel:SetTextAlignment(Turbine.UI.ContentAlignment.MiddleRight)
+diffXpLabel:SetFontStyle(Turbine.UI.FontStyle.Outline);
+diffXpLabel:SetForeColor(Turbine.UI.Color(0.64, 1, 0, 1))
+diffXpLabel:SetOutlineColor(Turbine.UI.Color(0, 0, 0))
+diffXpLabel:SetText('0')
+
+lastXp = 0
 
 
--- helper to register event callback
+-- helper to register event callbackaaa
 function AddCallback(object, event, callback)
 	if (object[event] == nil) then
 		object[event] = callback;
@@ -20,8 +55,20 @@ end
 
 AddCallback(Turbine.Chat, 'Received', function(sender, args)
 	if (args.ChatType == Turbine.ChatType.Advancement) then
+			-- elminiate any other messages that don't have to do with XP
+			if not string.match(args.Message, "You've earned [%d,]+ XP for a total of [%d,]+ XP.") then return end
+			
 			local totalXp = CalculateXpPercentage(ParseTotalXp(args.Message))
-			Turbine.Shell.WriteLine('Percent xp: ' ..string.format('%.4f', totalXp))
+			local diffXp = totalXp - lastXp
+			
+			lastXp = totalXp
+
+			local decimalPlacesTotalXp = string.format('%.4f', totalXp) ..' %'
+			local decimalPlacesDiffXp = '+ '.. string.format('%.4f', diffXp) ..' %'
+
+			totalXpLabel:SetText(decimalPlacesTotalXp)
+			diffXpLabel:SetText(decimalPlacesDiffXp)
+			-- Turbine.Shell.WriteLine('Percent xp: ' ..decimalPlacesXp)
         end
     end
 )
@@ -43,9 +90,6 @@ function CalculateXpPercentage(currentXp)
 
 	currrentLevelXp = LevelXpNeeded[player:GetLevel()]
 	nextLevelXp = LevelXpNeeded[player:GetLevel() + 1]
-
-	Turbine.Shell.WriteLine('Current lvl xp: ' ..(currentXp - currrentLevelXp))
-	Turbine.Shell.WriteLine('Next lvl  xp: ' ..(nextLevelXp - currrentLevelXp))
 
 	return ((currentXp - currrentLevelXp) / (nextLevelXp - currrentLevelXp)) * 100;
 end
